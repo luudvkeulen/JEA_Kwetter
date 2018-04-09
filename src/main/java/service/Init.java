f5cd44c5b2e0c7740ec2f5703ffde185cf8c1c90
Luud van Keulen <l.vankeulen@student.fontys.nl>
2018-04-09 10:54:33 +0200

    Added random pictures

--- src/main/java/service/Init.java
+++ src/main/java/service/Init.java
@@ -3,7 +3,6 @@ package service;
 import com.thedeanda.lorem.Lorem;
 import com.thedeanda.lorem.LoremIpsum;
 import dao.UserDAO;
-import domain.Tweet;
 import domain.User;
 import domain.UserGroup;
 import java.util.ArrayList;
@@ -29,13 +28,13 @@ public class Init {
         
         //Create and persist admin
         User admin = new User("admin@admin.com", "admin", "admin");
-        admin.setPicture("https://s-media-cache-ak0.pinimg.com/originals/73/be/49/73be498fcfc76c204e66686914de4beb.jpg");
+        admin.setPicture("lion.jpg");
         admin.addGroup(adminGroup);
         userDAO.insert(admin);
         
         //Create and persist moderator
         User moderator = new User("mod@mod.com", "moderator", "moderator");
-        moderator.setPicture("http://www.animalgalleries.org/albums/Large-Land-Mammals/Wolf/Grey%20Wolf%20Mexican%20Wolf%20Canis%20Lupus.jpg");
+        moderator.setPicture("wolf.jpg");
         moderator.addGroup(modGroup);
         userDAO.insert(moderator);
         
@@ -43,10 +42,11 @@ public class Init {
         final int usercount = 25;
         final int tweets = 30;
         
+        String[] pictures = new String[] {"dog.png", "bear.jpg", "horse.jpg", "sloth.jpg"};
         //Generate users;
         List<User> users = new ArrayList<>();
         for(int i = 0; i < usercount; i++) {
-            User tempUser = new User("test" + i + "@test.com", "test" + i, "12345678", "https://picsum.photos/300/300/?random", "test" + i + ".nl", "TestFN" + i, "TestLN" + 1, "Hello I am test" + i, "Eindhoven");
+            User tempUser = new User("test" + i + "@test.com", "test" + i, "12345678", pictures[new Random().nextInt(pictures.length)], "test" + i + ".nl", "TestFN" + i, "TestLN" + 1, "Hello I am test" + i, "Eindhoven");
             tempUser.addGroup(userGroup);
             userDAO.insert(tempUser);
             users.add(tempUser);
